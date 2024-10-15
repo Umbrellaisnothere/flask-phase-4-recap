29c7066034c2e0572376f233201160e22e602666
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-15 19:44:27 +0300

    Made the rest of the app.routes

--- app.py
+++ app.py
@@ -27,25 +27,66 @@ def users():
     if request.method == "GET":    
         users = User.query.all()
         response = [user.to_dict() for user in users]
+
         return make_response(response, 200)
-    elif request.method == "POST":
+    if request.method == "POST":
+        data = request.get_json()
+        new_user = User(username=data["username"], email=data["email"])
+        db.session.add(new_user)
+        db.session.commit()
 
+        return make_response(new_user.to_dict(), 201)
+    
 
-# @app.route("/posts", method = ["GET", "POST"])
-# def posts():
-#     if request.method == "GET":
-#         posts = Post.query.all()
-#         response = [post.to_dict() for post in posts]
-#         return make_response(response, 200)
-#     elif request.method == "POST":
-
-@app.route("/posts/<int:id>", methods=["GET", "POST"])
-def posts(id):
+@app.route("/users/<int:id>", methods=["GET", "PATCH"])
+def user_detail(id):
+    user = User.query.get(id)
+    if not user:
+        return make_response({"error": "User not found"}, 404)
+    
     if request.method == "GET":
-        post = Post.query.all()
+        return make_response(user.to_dict(), 200)
+
+    if request.method == "PATCH":
+        data = request.get_json()
+        user.username = data.get("username", user.username)
+        user.email = data.get("email", user.email)
+        db.session.commit()
+        return make_response(user.to_dict(), 200)
+
+
+
+@app.route("/posts", methods= ["GET", "POST"])
+def posts():
+    if request.method == "GET":
+        posts = Post.query.all()
         response = [post.to_dict() for post in posts]
+
         return make_response(response, 200)
-    elif request.method == "POST":
+    if request.method == "POST":
+        data = request.get_json()
+        new_post = Post(title=data["title"], content=data["content"])
+        db.session.add(new_post)
+        db.session.commit()
+
+        return make_response(new_post.to_dict(), 201)
+
+
+@app.route("/posts/<int:id>", methods=["GET", "PATCH"])
+def post_detail(id):
+    post = Post.query.get(id)
+    if not post:
+        return make_response({"error": "Post not found"}, 404)
+
+    if request.method == "GET":
+        return make_response(post.to_dict(), 200)
+
+    if request.method == "PATCH":
+        data = request.get_json()
+        post.title = data.get("title", post.title)
+        post.content = data.get("content", post.content)
+        db.session.commit()
+        return make_response(post.to_dict(), 200)
 
         
 
