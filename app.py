b3254e29e12155802249e53537c57e1a8291b4ef
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-15 09:54:37 +0300

    Initialized a migrate

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_migrate import Migrate
+from models import db
 
 # Flask is a lightweight Python web framework for building web applications
 # Flask is a micro-framework, which means it does not include a lot of features
 # Flask is designed to be simple and easy to get started with
 
-Database = "sqlite:///recap.db"
+# Database = "sqlite:///recap.db"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URL"] = Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recap.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-migrate = Migrate(app)
+migrate = Migrate(app, db)
+db.init_app(app)
+
 
 @app.route("/")
 def index():
     return "<h1>Welcome to Flask</h1>"
-
 
 if __name__ == '__main__':
     app.run(port=5500, debug=True)
