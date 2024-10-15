482c845dd6d1bd979d9c9009bb745f97315f64f7
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-15 10:52:08 +0300

    Scraped the migrations and recap.db file and re-initialized the process

--- models.py
+++ models.py
@@ -9,5 +9,11 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    
+    email = db.Column(db.String(120), nullable=False, unique=True)
 
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(200))
