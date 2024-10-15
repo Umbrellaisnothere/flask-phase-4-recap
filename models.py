f0f9fc4322e461dcb086d60e7f062212e6e07b99
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-15 10:24:26 +0300

    Updated the migrations version folder and upgraded to the database

--- models.py
+++ models.py
@@ -2,3 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    
+
